dac157fd117485935a84a44c8a8b74f5c9c4d51c
Hamna Asif <hamnaasif7377@gmail.com>
2025-09-14 02:37:26 +0500

    Errors corrected in files

--- app/src/main/java/com/example/hamnaapptask/twelve.kt
+++ app/src/main/java/com/example/hamnaapptask/twelve.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class twelve : AppCompatActivity() {
+class Twelve : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
